9d3865b82ef9ca245da92cf8acd1c16e71271758
Sam Bowen <Samuel_bowen_1994@hotmail.co.uk>
2018-10-02 12:59:44 +0100

    Completed tutorial 2

--- Tut2 - Functions/Tut2 - Functions/Tut2 - Functions.cpp
+++ Tut2 - Functions/Tut2 - Functions/Tut2 - Functions.cpp
@@ -4,21 +4,74 @@
 #include "stdafx.h"
 #include <iostream>
 
-int main()
-{
+using namespace std;
 
+int add(int x, int y) {
 
-
-
-
-
-
-
-
-
-
-
-
-    return 0;
+	return x + y;
 }
 
+int minus(int x, int y);
+
+void product(int x, int y); //task 1
+
+double quotient(double x, int y); //task 2
+
+void calc(double x, double y, char op); //task 3
+
+int main() {
+
+	int a, b, c, exit;
+
+	cout << "Please type in a number" << "\n";
+	cin >> a;
+
+	cout << "Please type in another number" << "\n";
+	cin >> b;
+
+	c = add(a, b);
+
+	cout << a << '+' << b << '=' << c << "\n";
+
+	//Task 1 output
+	product(1, 2);
+
+	//Task 2 output
+	double quot = quotient(5, 3);
+
+	//Task 3 output
+	calc(2, 3, '-');
+
+	cout << "enter another number to exit" << "\n";
+
+	cin >> exit;
+
+	return 0;
+}
+
+int minus(int x, int y) {
+
+	return x - y;
+}
+
+//Task 1
+void product(int x, int y) {
+	cout << "Product of " << x << " + " << y << " = " << x + y << endl;
+}
+
+//Task 2
+double quotient(double x, int y) {
+	cout << "The quotient of " << x << " / " << y << " = " << x/y << endl;
+	return x/y;
+}
+
+//Task 3
+void calc(double x, double y, char op) {
+	switch (op) {
+	case '+': cout << "Product of " << x << " + " << y << " = " << x + y << endl; break;
+	case '-': cout << "Product of " << x << " - " << y << " = " << x - y << endl; break;
+	case '*': cout << "Product of " << x << " * " << y << " = " << x * y << endl; break;
+	case '/': cout << "Product of " << x << " / " << y << " = " << x / y << endl; break;
+	default: cout << "Invalid operator" << endl; break;
+	}
+}
