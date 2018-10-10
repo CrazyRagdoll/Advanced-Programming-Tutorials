0fc220fa03c853e9b5b253b47f324db38ee5a393
Sam Bowen <Samuel_bowen_1994@hotmail.co.uk>
2018-10-10 15:37:07 +0100

    Added tutorial 11 and completed task 1 & 2

---
+++ Tut11 - Templates/Tut11 - Templates/Tut11 - Templates.cpp
@@ -0,0 +1,19 @@
+// Tut11 - Templates.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
+/*	Task 1
+	The compiler performs the compilation of a template class when it has been instantiated with the type argument specifying the template type. 
+*/
+
+/*	Task 2
+	When the compiler generates the code for a template it needs all of the informaton regarding which describes the functionality of that template in the same place. Therefore you 
+	need to make all of the code pertaining to the template availible in the same header file.
+*/
